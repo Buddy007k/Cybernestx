396b0ac53dd9ded7d1929fb0b414ba13b6f28f89
Chetan <chetan.gautam1210@gmail.com>
2026-04-22 14:41:18 +0530

    final project

--- app/api/admin/messages/[id]/route.js
+++ app/api/admin/messages/[id]/route.js
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/db";
-import Contact from "@/models/contact";
+import Contact from "@/models/Contact";
 
 export async function DELETE(req, { params }) {
   await connectDB();
