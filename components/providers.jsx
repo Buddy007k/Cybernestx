489005251e652e6089cc59ccf06dec18e984be88
Chetan <chetan.gautam1210@gmail.com>
2026-04-27 16:51:23 +0530

    default theme

--- components/providers.jsx
+++ components/providers.jsx
@@ -9,7 +9,7 @@ export default function Providers({ children }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem={true}
     >
       <CursorGlow />
